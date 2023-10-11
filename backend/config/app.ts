85c4dfadf3e0b87f1ce097bd2e0ec7e1baa20003
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-10-11 10:54:42 -0300

    🎉 start backend project

---
+++ backend/config/app.ts
@@ -0,0 +1,46 @@
+import proxyAddr from "proxy-addr";
+import Env from "@ioc:Adonis/Core/Env";
+import type { ServerConfig } from "@ioc:Adonis/Core/Server";
+import type { LoggerConfig } from "@ioc:Adonis/Core/Logger";
+import type { ProfilerConfig } from "@ioc:Adonis/Core/Profiler";
+import type { ValidatorConfig } from "@ioc:Adonis/Core/Validator";
+
+export const appKey: string = Env.get("APP_KEY");
+
+export const http: ServerConfig = {
+  allowMethodSpoofing: false,
+  subdomainOffset: 2,
+  generateRequestId: false,
+
+  trustProxy: proxyAddr.compile("loopback"),
+
+  etag: false,
+
+  jsonpCallbackName: "callback",
+
+  cookie: {
+    domain: "",
+    path: "/",
+    maxAge: "2h",
+    httpOnly: true,
+    secure: false,
+    sameSite: false,
+  },
+
+  forceContentNegotiationTo: "application/json",
+};
+
+export const logger: LoggerConfig = {
+  name: Env.get("APP_NAME"),
+  enabled: true,
+  level: Env.get("LOG_LEVEL", "info"),
+  prettyPrint: Env.get("NODE_ENV") === "development",
+};
+
+export const profiler: ProfilerConfig = {
+  enabled: true,
+  blacklist: [],
+  whitelist: [],
+};
+
+export const validator: ValidatorConfig = {};
