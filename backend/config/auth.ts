f64c4fd759f38ec156f4f9ddad910cd5f6b4f1c7
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-10-13 11:36:43 -0300

    ✨ configured user, auth, token

---
+++ backend/config/auth.ts
@@ -0,0 +1,28 @@
+import type { AuthConfig } from "@ioc:Adonis/Addons/Auth";
+
+const authConfig: AuthConfig = {
+  guard: "api",
+  guards: {
+    api: {
+      driver: "oat",
+      tokenProvider: {
+        type: "api",
+        driver: "database",
+        table: "api_tokens",
+        foreignKey: "user_id",
+      },
+
+      provider: {
+        driver: "lucid",
+
+        identifierKey: "id",
+
+        uids: ["username"],
+
+        model: () => import("App/Models/User"),
+      },
+    },
+  },
+};
+
+export default authConfig;
