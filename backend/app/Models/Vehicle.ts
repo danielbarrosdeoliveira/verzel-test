f1dea812fd668bf4dbbd424d4b4a8e1df7a59820
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-10-15 12:32:12 -0300

    ✨ created model Vehicle

---
+++ backend/app/Models/Vehicle.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Vehicle extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public brand: string
+
+  @column()
+  public model: string
+
+  @column()
+  public photo: string
+
+  @column()
+  public value: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
