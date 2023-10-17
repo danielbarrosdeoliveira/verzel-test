08e20fac17905a9188d0c1883640265ce1392057
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-10-17 14:03:17 -0300

    ✨ created data vehicles table to show grid on main page

---
+++ frontend/lib/components/vehicle/data_table.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/models/vehicle_model.dart';
+import 'package:frontend/utils/vehicle.dart';
+
+Widget buildVehicleDataTable(
+  double width,
+  Function(BuildContext, Vehicle?) openVehicleFormModal,
+  List<Vehicle> vehicles,
+  BuildContext context,
+  Function(BuildContext, Vehicle?) openDeleteVehicleModel,
+) {
+  return SizedBox(
+    width: width,
+    height: MediaQuery.of(context).size.height / 1.2,
+    child: SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: DataTable(
+        columns: const [
+          DataColumn(label: Text('Foto')),
+          DataColumn(label: Text('Marca')),
+          DataColumn(label: Text('Modelo')),
+          DataColumn(label: Text('Preço')),
+          DataColumn(label: Text('Ações')),
+        ],
+        rows: vehicles.map((vehicle) {
+          return DataRow(cells: [
+            DataCell(
+              SizedBox(
+                width: 100,
+                child: Image.asset(
+                  vehicle.photo,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            DataCell(Text(vehicle.brand)),
+            DataCell(Text(vehicle.model)),
+            DataCell(
+              Text(
+                priceFormat.format(vehicle.price),
+              ),
+            ),
+            DataCell(Row(
+              children: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.edit,
+                    color: Colors.amber,
+                  ),
+                  onPressed: () {
+                    openVehicleFormModal(context, vehicle);
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.red,
+                  ),
+                  onPressed: () {
+                    openDeleteVehicleModel(context, vehicle);
+                  },
+                ),
+              ],
+            )),
+          ]);
+        }).toList(),
+      ),
+    ),
+  );
+}
