4fc31f3ea40df7448e4f9c6dfd7c2a02ccf7f117
Daniel Oliveira <d.dani_oli@hotmail.com>
2023-10-18 10:56:05 -0300

    ♻️

--- frontend/lib/pages/page_main.dart
+++ frontend/lib/pages/page_main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:frontend/components/card-vehicle/card_vehicle.dart';
 import 'package:frontend/components/shared/responsive.dart';
-import 'package:frontend/mocks/vehicle.dart';
 import 'package:frontend/models/vehicle_model.dart';
 import 'package:frontend/pages/page_login.dart';
+import 'package:frontend/services/vehicle_services.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -14,13 +14,48 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  List<Vehicle> vehicles = getMockVehicles();
+  List<Vehicle>? vehicles;
+  bool isLoading = false;
+
+  VehicleService vehicleService = VehicleService();
+
+  @override
+  void initState() {
+    super.initState();
+    handleGetVehicles();
+  }
+
+  handleGetVehicles() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+
+      var result = await vehicleService.index();
+
+      if (result != null) {
+        setState(() {
+          vehicles = result;
+        });
+      }
+      return;
+    } catch (e) {
+      print(e);
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Vertel Veículos Usados - Listagem de Veículos"),
+        title: const Text(
+          "Vertel Veículos Usados",
+        ),
+        centerTitle: true,
         actions: [
           ElevatedButton(
             style: const ButtonStyle(elevation: MaterialStatePropertyAll(0)),
@@ -43,102 +78,122 @@ class _MainPageState extends State<MainPage> {
   }
 
   Widget _buildContent(BuildContext context) {
-    if (ResponsiveWidget.isExtraLargeScreen(context)) {
-      return Padding(
-        padding: const EdgeInsets.all(16),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 5,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 1,
-          ),
-          itemCount: vehicles.length,
-          itemBuilder: (context, index) {
-            final vehicle = vehicles[index];
-            return CardVehicle(
-              vehicle: vehicle,
-            );
-          },
+    if (isLoading) {
+      return const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(height: 16),
+            Text('Carregando...'),
+          ],
         ),
       );
-    } else if (ResponsiveWidget.isLargeScreen(context)) {
-      return Padding(
-        padding: const EdgeInsets.all(16),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 1,
-          ),
-          itemCount: vehicles.length,
-          itemBuilder: (context, index) {
-            final vehicle = vehicles[index];
-            return CardVehicle(
-              vehicle: vehicle,
-            );
-          },
-        ),
-      );
-    } else if (ResponsiveWidget.isMediumScreen(context)) {
-      return Padding(
-        padding: const EdgeInsets.all(8),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            crossAxisSpacing: 8,
-            mainAxisSpacing: 8,
-            childAspectRatio: 1,
-          ),
-          itemCount: vehicles.length,
-          itemBuilder: (context, index) {
-            final vehicle = vehicles[index];
-            return CardVehicle(
-              vehicle: vehicle,
-            );
-          },
-        ),
-      );
-    } else if (ResponsiveWidget.isSmallScreen(context)) {
-      return Padding(
-        padding: const EdgeInsets.all(8),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 8,
-            mainAxisSpacing: 8,
-            childAspectRatio: 1,
-          ),
-          itemCount: vehicles.length,
-          itemBuilder: (context, index) {
-            final vehicle = vehicles[index];
-            return CardVehicle(
-              vehicle: vehicle,
-            );
-          },
+    } else if (vehicles == null) {
+      return const Center(
+        child: Text(
+          "Nenhum Veículo Cadastrado...\nFaça login para cadastrar novos veículos!",
+          style: TextStyle(fontSize: 18),
         ),
       );
     } else {
-      // Extra Small Screen
-      return Padding(
-        padding: const EdgeInsets.all(16),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 1,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 1,
+      if (ResponsiveWidget.isExtraLargeScreen(context)) {
+        return Padding(
+          padding: const EdgeInsets.all(16),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 5,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              childAspectRatio: 1,
+            ),
+            itemCount: vehicles!.length,
+            itemBuilder: (context, index) {
+              final vehicle = vehicles![index];
+              return CardVehicle(
+                vehicle: vehicle,
+              );
+            },
           ),
-          itemCount: vehicles.length,
-          itemBuilder: (context, index) {
-            final vehicle = vehicles[index];
-            return CardVehicle(
-              vehicle: vehicle,
-            );
-          },
-        ),
-      );
+        );
+      } else if (ResponsiveWidget.isLargeScreen(context)) {
+        return Padding(
+          padding: const EdgeInsets.all(16),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              childAspectRatio: 1,
+            ),
+            itemCount: vehicles!.length,
+            itemBuilder: (context, index) {
+              final vehicle = vehicles![index];
+              return CardVehicle(
+                vehicle: vehicle,
+              );
+            },
+          ),
+        );
+      } else if (ResponsiveWidget.isMediumScreen(context)) {
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 1,
+            ),
+            itemCount: vehicles!.length,
+            itemBuilder: (context, index) {
+              final vehicle = vehicles![index];
+              return CardVehicle(
+                vehicle: vehicle,
+              );
+            },
+          ),
+        );
+      } else if (ResponsiveWidget.isSmallScreen(context)) {
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 1,
+            ),
+            itemCount: vehicles!.length,
+            itemBuilder: (context, index) {
+              final vehicle = vehicles![index];
+              return CardVehicle(
+                vehicle: vehicle,
+              );
+            },
+          ),
+        );
+      } else {
+        // Extra Small Screen
+        return Padding(
+          padding: const EdgeInsets.all(16),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              childAspectRatio: 1,
+            ),
+            itemCount: vehicles!.length,
+            itemBuilder: (context, index) {
+              final vehicle = vehicles![index];
+              return CardVehicle(
+                vehicle: vehicle,
+              );
+            },
+          ),
+        );
+      }
     }
   }
 }
